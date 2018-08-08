65145522a06f5b28f04ac1c3e6a42366c3150f8c
hucongcong <flycc258@163.com>
2018-08-08 23:29:49 +0800

    初始化提交

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <router-link to="/">Hello world</router-link>
+    <router-link to="/home">home</router-link>
+    <router-link to="/user">user</router-link>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
